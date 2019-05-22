f58d795a5743fd6d6ff854864a7e2b15ff3b93eb
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-22 17:26:44 +0200

    feat(teanat): Show data from item model

--- app/Http/Controllers/Api/TenantController.php
+++ app/Http/Controllers/Api/TenantController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Api\Item;
 
 class TenantController extends Controller
 {
     public function show()
     {
-        dd('hhhhh');
+        return Item::all();
     }
 }
