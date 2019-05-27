9d788d3816151bbd2efee3d6a09c1f75e3d31c94
a-tarek <a.tarek.elsayed@gmail.com>
2019-05-27 17:52:24 +0200

    Update api key check middleware

--- app/Http/Middleware/ApiKeyCheck.php
+++ app/Http/Middleware/ApiKeyCheck.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\Subscription;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 use App\Library\Response\JsonResponse;
 // function fail($code = 400)
 // {
@@ -25,18 +27,19 @@ class ApiKeyCheck
      */
     public function handle($request, Closure $next)
     {
-        return !($this->header_key($request) === $this->subs_key()) ? $this->invalid() : $next($request);
+        return !($this->header_key($request) === $this->cached_key($request)) ? $this->invalid() : $next($request);
     }
 
     public function invalid(){
         return response()->json(['key'=>'Invalid API Key was given']);
     }
-
+    
     public function header_key($request){
         return isset($request->header()['key'])?  $request->header()['key'][0] : false;
     }
-
-    public function subs_key(){
-        return Subscription::all()->first()->api_key;
+    
+    public function cached_key( $request){
+        // get the redis cached value from the created subsription 
+        return Redis::get($request->header()['host'][0]);
     }
 }
