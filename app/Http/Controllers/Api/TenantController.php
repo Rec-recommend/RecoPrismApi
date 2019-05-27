46ddc20004a1c4729e5afc7179dbe0cfe6375687
a-tarek <a.tarek.elsayed@gmail.com>
2019-05-27 17:54:53 +0200

    Refactor controller

--- app/Http/Controllers/Api/TenantController.php
+++ app/Http/Controllers/Api/TenantController.php
@@ -7,21 +7,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Factories\TenantModelFactory;
-use Hyn\Tenancy\Traits\UsesTenantConnection;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Config;
+use Hyn\Tenancy\Traits\UsesTenantConnection;
 
 class TenantController extends Controller
 {
     use UsesTenantConnection;
-    // use JsonResponse;
 
-    public function __construct()
-    {
-        // $this->middleware('check.apikey');
-        // $this->middleware('tenancy.enforce');
-    }
+    public function __construct(){ }
+    
     public function insert_data(Request $request)
     {
-            return response()->json(['message'=>'data added'],200);
+        return response()->json(['message'=>'data added'],200);
     }
 }
