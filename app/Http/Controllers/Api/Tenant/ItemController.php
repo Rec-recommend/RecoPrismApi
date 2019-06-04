bbb0242e8328e50f6e98d059af02fd83fe8cbd0c
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-04 22:49:56 +0200

    Remove EntityRepository

--- app/Http/Controllers/Api/Tenant/ItemController.php
+++ app/Http/Controllers/Api/Tenant/ItemController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers\Api\Tenant;
 
+use Illuminate\Http\Request;
 use App\Repositories\Tenant\ItemRepository;
 
-class ItemController extends EntityController
+class ItemController
 {
     public function __construct(ItemRepository $item)
     {
-        $this->entity = $item;
+        $this->repository = $item;
+    }
+
+    public function store(Request $request)
+    {
+        // if ($request->hasFile('csvfile')) {
+        //     $path = $request->file('csvfile')->getRealPath();
+        //     $array = array_map('str_getcsv',file($path));
+        // }
+
+        $this->repository->store($request->all());
+        return response()->json([
+            'message' => "Data added successfully"
+        ], 201);
     }
 }
--- app/Repositories/Tenant/UserRepository.php
+++ app/Repositories/Tenant/UserRepository.php
@@ -5,11 +5,5 @@ use App\Models\Tenant\EntityRepository;
 
 class UserRepository  extends EntityRepository
 {
-    public function __construct()
-    {
-        $this->entity_id = 2;
-        $this->entity_name = "user";
-        $this->entity_model = \App\Models\Tenant\User::class;
-        $this->entity_eav = \App\Models\Tenant\UAV::class;
-    }
+    public function __construct(){ }
 }
