b083ad538496490f414b98738ae92f3a4899ab0a
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-04 03:18:28 +0200

    Store items with raw sql in EntityRepositoty

--- app/Repositories/Tenant/EntityRepository.php
+++ app/Repositories/Tenant/EntityRepository.php
@@ -7,22 +7,11 @@ use Illuminate\Support\Facades\DB;
 
 abstract class EntityRepository
 {
-    protected $entity_id; //id
-    protected $entity_name; // string
-    protected $entity_model;  //class
-    protected $entity_eav; // class
-
-    protected function __construct()
-    {
-    }
-
-    public function all()
-    {
-
-    }
-
     public function store($items)
     {
+        dd($items);
+        $iav_sql  = "insert into iav (`item_id`, `attribute_id`, `value`) values ";
+        $item_sql = "insert into items (`id`) values ";
         $eavs = [];
         $item_ids = [];
         $attr_label_to_id = [];
@@ -32,59 +21,28 @@ abstract class EntityRepository
         }
 
         foreach ($items as $item) {
-            $item_ids [] = [ 'id'=> $item['id'] ];
+            $item_sql .= "('" . $item['id'] . "'), ";
             foreach ($item as $attr_label => $attr_value) {
                 if($attr_label !== "id"){
-                    $eavs [ ] = [
-                        "item_id"=> $item["id"],
-                        'attribute_id'=>$attr_label_to_id[$attr_label],
-                        'value'=>$attr_value
-                    ];
+                    $iav_sql .= " (";
+                    $iav_sql .= " '" . addslashes($item["id"])                    . "', ";
+                    $iav_sql .= " '" . addslashes($attr_label_to_id[$attr_label]) . "', ";
+                    $iav_sql .= " '" . addslashes($attr_value)                    . "' ";
+                    $iav_sql .= "), ";
                 }
             }
         }
-        Item::insert($item_ids);
-        IAV::insert($eavs);
+
+        $iav_sql  = substr($iav_sql, 0, -2);
+        $item_sql = substr($item_sql, 0, -2);
+        DB::statement($item_sql);
+        DB::statement($iav_sql);
     }
 }
 
 /*
     [
-        { 'id':3,'title':'iphone','price':1000 } ===> 3,2(attr_id),'iphone'  --- 3,4(attr_id),1000
+        { 'id':1,'title':'iphone','price':1000 },
+        { 'id':2,'title':'samsung','price':2000 },
     ]
-        [title => title_id in attributes table, ]
 */
-
-        // $iav_sql  = "insert into iav";
-        // $item_sql = "insert into items (id) values";
-
-        // $test = [1,2,3];
-        // dd($test);
-
-        // $ids=[];
-        // $sql = "insert into items (id) values";
-        // $sql = $sql . "('" . $id . "'), ";
-        // foreach($item_ids as $id_array){
-        //     foreach($id_array as $id){
-        //         $sql = $sql . "('" . $id . "'), ";
-        //     }
-        // }
-        // $sql = substr($sql, 0, -2);
-
-        // DB::statement($sql);
-        // DB::insert('insert into iav (id, title, genres) values (?,?,?)', $eavs);
-
-
-// $ids=[];
-// $sql = 'insert into items (id) values';
-// foreach($entity_ids as $id_ass_array){
-//     foreach($id_ass_array as $id){
-//         $ids[]=$id;
-//         $sql = $sql . ' (?), ';
-//     }
-// }
-// $sql = substr($sql, 0, -2);
-// $ids = implode(",", $ids);
-// var_dump($sql);
-// var_dump($ids);
-// DB::insert($sql, $ids);
