a41568d1f5b4e8657b94351e021a04120cc27368
Mira <mira.ph.ling@gmail.com>
2019-06-09 20:35:19 +0200

    Modify  Navbar

--- resources/views/guest/landingPage.blade.php
+++ resources/views/guest/landingPage.blade.php
@@ -17,40 +17,7 @@
   <link rel="stylesheet" href="{{ asset('guest') }}/css/style.css">
 </head>
 <body>
-  <!--================Header Menu Area =================-->
-  <header class="header_area">
-    <div class="main_menu">
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container box_1620">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <a class="navbar-brand logo_h" href="/"><h2 class="text-white">RecoPrism</h2></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-            <ul class="nav navbar-nav menu_nav justify-content-end">
-              <li class="nav-item active"><a class="nav-link" href="/">Home</a></li> 
-              <li class="nav-item"><a class="nav-link" href="#feature">Features</a></li> 
-              <li class="nav-item"><a class="nav-link" href="#pricing">Price</a>
-              <li class="nav-item"><a class="nav-link" href="#us">About Us</a></li>
-              <li class="nav-item"><a class="nav-link" href="#">Docs</a></li>
-
-            </ul>
-
-            <ul class="navbar-right">
-              <li class="nav-item">
-                <a href="{{route('ClientCreate')}}"class="button button-header bg">Sign up</a>
-              </li>
-            </ul>
-          </div> 
-        </div>
-      </nav>
-    </div>
-  </header>
-  <!--================Header Menu Area =================-->
+@include('layouts.navbars.navs.landing')
 
 
   <main class="side-main">
--- resources/views/guest/register.blade.php
+++ resources/views/guest/register.blade.php
@@ -17,43 +17,9 @@
 </head>
 <body>
   <!--================Header Menu Area =================-->
-  <header class="header_area">
-    <div class="main_menu">
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container box_1620">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <a class="navbar-brand logo_h" href="/"><h2 class="text-white">RecoPrism</h2></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-            <ul class="nav navbar-nav menu_nav justify-content-end">
-              <li class="nav-item active"><a class="nav-link" href="/">Home</a></li> 
-              <li class="nav-item"><a class="nav-link" href="{{ asset('guest') }}/feature.html">Feature</a></li> 
-              <li class="nav-item"><a class="nav-link" href="#pricing">Price</a>
-              <li class="nav-item submenu dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                  aria-expanded="false">Pages</a>
-                <ul class="dropdown-menu">
-                  <li class="nav-item"><a class="nav-link" href="{{ asset('guest') }}/blog.html">Blog</a></li>
-                  <li class="nav-item"><a class="nav-link" href="{{ asset('guest') }}/blog-details.html">Blog Details</a></li>
-                </ul>
-							</li>
-              <li class="nav-item"><a class="nav-link" href="{{ asset('guest') }}/contact.html">Contact</a></li>
-            </ul>
-            <ul class="navbar-right">
-              <li class="nav-item">
-                <button class="button button-header bg">Sign up</button>
-              </li>
-            </ul>
-          </div> 
-        </div>
-      </nav>
-    </div>
-  </header>
+ 
+  @include('layouts.navbars.navs.landing')
+
     <!--================ Hero sm Banner start =================-->      
     <section class="hero-banner mb-20px">
       <div class="container">
