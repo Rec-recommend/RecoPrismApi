f7e8145d9d1858cafebbbccab928279d5173ef7b
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-06-09 21:00:58 +0200

    Edit API Key check middleware

--- app/Http/Middleware/ApiKeyCheck.php
+++ app/Http/Middleware/ApiKeyCheck.php
@@ -7,6 +7,8 @@ use App\Subscription;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
 use App\Library\Response\JsonResponse;
+use App\Models\Tenant\Setting;
+
 // function fail($code = 400)
 // {
     //     return new Fail($code);
@@ -27,19 +29,23 @@ class ApiKeyCheck
      */
     public function handle($request, Closure $next)
     {
-        return !($this->header_key($request) === $this->cached_key($request)) ? $this->invalid() : $next($request);
+        return !($this->header_key($request) === $this->db_key()) ? $this->invalid() : $next($request);
     }
 
     public function invalid(){
-        return response()->json(['key'=>'Invalid API Key was given']);
+        return response()->json(['API_KEY'=>'Invalid API Key was given']);
     }
     
     public function header_key($request){
-        return isset($request->header()['key'])?  $request->header()['key'][0] : false;
+        return isset($request->header()['API_KEY'])?  $request->header()['API_KEY'][0] : false;
     }
     
     public function cached_key( $request){
         // get the redis cached value from the created subsription 
         return Redis::get($request->header()['host'][0]);
+    }
+
+    public function db_key(){
+        return Setting::where('key','API_KEY')->first()['value'];
     }
 }
