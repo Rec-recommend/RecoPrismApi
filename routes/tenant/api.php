7f9b019c6f60b529789d0410593762b29f879641
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-09 20:56:07 +0200

    Add Recommendation resource

--- routes/tenant/api.php
+++ routes/tenant/api.php
@@ -6,3 +6,5 @@ Route::apiResource('items', 'Tenant\Api\ItemController');
 Route::apiResource('users', 'Tenant\Api\UserController');
 Route::apiResource('ratings', 'Tenant\Api\RatingController');
 Route::apiResource('purchases', 'Tenant\Api\PurchaseController');
+Route::get('/recommendations/users/{id}', 'Tenant\Api\RecommendationController@recos')->name('recos.users');
+Route::get('/recommendations/items/{id}', 'Tenant\Api\RecommendationController@recos')->name('recos.items');
