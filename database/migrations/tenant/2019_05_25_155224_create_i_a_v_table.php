3f52cb7fda8291c331cf2665e0919cc4d26f058e
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-06-10 20:55:32 +0200

    Alter primary key on iavs table

--- database/migrations/tenant/2019_05_25_155224_create_i_a_v_table.php
+++ database/migrations/tenant/2019_05_25_155224_create_i_a_v_table.php
@@ -14,15 +14,15 @@ class CreateIAVTable extends Migration
     public function up()
     {
         Schema::create('iav', function (Blueprint $table) {
-            $table->bigIncrements('id');
             $table->string('value');
+            
+            $table->bigInteger('item_id')->unsigned();            
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade')->onUpdate('cascade');
 
-            $table->bigInteger('item_id')->unsigned()->nullable();
-            $table->foreign('item_id')->references('id')->on('items')->nullable();
-
-            $table->bigInteger('attribute_id')->unsigned()->nullable();
-            $table->foreign('attribute_id')->references('id')->on('attributes')->onDelete('set null');
-
+            $table->bigInteger('attribute_id')->unsigned();
+            $table->foreign('attribute_id')->references('id')->on('attributes')->onDelete('cascade')->onUpdate('cascade');
+            $table->primary(['item_id','attribute_id']);
+            
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
         });
