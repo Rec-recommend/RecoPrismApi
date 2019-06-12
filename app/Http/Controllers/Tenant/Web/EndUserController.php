0919e05c356a474051f6294f587c1bd0a8c01f5c
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-06-12 22:07:10 +0200

    Add searching function for user by id

--- app/Http/Controllers/Tenant/Web/EndUserController.php
+++ app/Http/Controllers/Tenant/Web/EndUserController.php
@@ -17,4 +17,14 @@ class EndUserController extends Controller
         $end_users = DB::table('end_users')->paginate($perPage);
         return view('tenant/endusers')->with('end_users', $end_users);
     }
+    public function search()
+    {
+        $inputID = Input::get('userID');
+        $end_user = EndUser::where('id', $inputID)->first();
+        if ($end_user) {
+            return view('tenant/userrecomendation')->with('end_user', $end_user);
+        } else {
+            return view('tenant/userrecomendation')->with('nfMsg', 'No Details found. Try to search again !');
+        }
+    }
 }
