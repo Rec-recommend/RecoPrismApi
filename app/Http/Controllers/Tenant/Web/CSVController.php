b5a5e6d8176e346d70639013ad30d5b115fd80d5
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-13 22:18:45 +0200

    Fix error message

--- app/Http/Controllers/Tenant/Web/CSVController.php
+++ app/Http/Controllers/Tenant/Web/CSVController.php
@@ -21,9 +21,9 @@ class CsvController
         $path = $request->file('csvfile')->getRealPath();
         $array = array_map('str_getcsv', file($path));
         $filtered = array_filter(array_map('array_filter', $array));
-        return ($repository->store($filtered))? 
+        return $repository->store($filtered)?
         redirect('csv')->with(['message'=> 'Data Uploaded Successfully!','success' => true]):
-        redirect('csv')->with(['message'=> 'Data does not match the required format, please check the documentation','false' => true]);
+        redirect('csv')->with(['message'=> 'Data does not match the required format, please check the documentation']);
     }
 
     public function import(){
