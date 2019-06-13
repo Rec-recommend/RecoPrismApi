037531daabb22af980b704a27fadbf8c3dab5cc8
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-06-14 00:11:41 +0200

    Fix search recommendations

--- resources/views/tenant/endusers.blade.php
+++ resources/views/tenant/endusers.blade.php
@@ -1,28 +1,37 @@
-@extends('layouts.app')
+@extends('layouts.app', ['title' => __('Search For User Recommendations')])
 
 @section('content')
-@include('layouts.headers.cards')
-<br>
-  
-<div class="container">
-    @if(Session::has('message'))
-        <p class="alert {{ Session::get('alert-class', 'alert-danger') }}">{{ Session::get('message') }}</p>
-    @endif
-<form action='{{ route('search')}}' method="GET" role="search">
-    {{ csrf_field() }}
-    <div class="container">
-    <div class="row ">
-        <input type="number"  min="1" class="form-control col-6" name="userID" placeholder="Search User ID"> 
-        <span class="input-group-btn">
-            <button class="btn btn-icon btn-3 btn-default" type="submit">
-	            <span class="btn-inner--icon"><i class="ni ni-bag-17"></i></span>
-                <span class="btn-inner--text">SEARCH</span>
-            </button>
-        </span>
+@include('users.partials.header', ['title' => __('User Recommendations')])
+
+<div class="container-fluid mt--7">
+    <div class="row">
+        <div class="col-xl-12 order-xl-1">
+            <div class="card bg-secondary shadow">
+                <div class="card-header bg-white border-0">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h3 class="mb-0">{{ __('Search For User Recommendations') }}</h3>
+                            @if(Session::has('message'))
+                            <p class="alert {{ Session::get('alert-class', 'alert-danger') }}">{{ Session::get('message') }}</p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action='{{ route('search')}}' method="GET" role="search">
+                        {{ csrf_field() }}
+                        <div class="container">
+                        <div class="row ">
+                            <input type="number"  min="1" class="form-control col-6" name="userID" placeholder="Search User ID"> 
+                        </div>
+                            <button type="submit" class="btn btn-success mt-4">{{ __('Search') }}</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
-</form>
+
+    @include('layouts.footers.auth')
 </div>
-<br>
-@include('layouts.footers.auth')
 @endsection
