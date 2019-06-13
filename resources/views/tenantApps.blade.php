36ca2d0e4f2be9b7b10337cc1b444d1947da8a31
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-06-13 17:06:27 +0200

    Modify System Admin Views

--- resources/views/tenantApps.blade.php
+++ resources/views/tenantApps.blade.php
@@ -1,50 +1,88 @@
-@extends('layouts.app')
+@extends('layouts.app', ['title' => __('Tenants Management')])
 
 @section('content')
     @include('layouts.headers.cards')
-                        <!-- Projects table -->
+
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <h3 class="mb-0">{{ __('Tenants') }}</h3>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-12">
+                        @if (session('status'))
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                {{ session('status') }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @endif
+                    </div>
+
+                    <div class="table-responsive">
                         <table class="table align-items-center table-flush">
                             <thead class="thead-light">
                                 <tr>
-                                    <th scope="col">Website Name</th>
-                                    <th scope="col">Maintenance Mode</th>
-                                    <th scope="col">Delete Website</th>
-
-                          
+                                    <th scope="col">{{ __('Website Name') }}</th>
+                                    <th scope="col">{{ __('Maintenance Mode') }}</th>
+                                    <th scope="col"></th>
                                 </tr>
                             </thead>
                             <tbody>
-                            @foreach ($hostnames as $hostname)
-                                <tr>
-                                    <th scope="row">
-                                        {{$hostname->fqdn}}
-                                    </th>
-                                    <th>
-                                        <form id="suspend-form" method="POST" action='{{route('toggleApp',[$hostname->id])}}'>
-                                            {{ csrf_field() }}
-                                            @if(!$hostname->under_maintenance_since)
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-success"> ON </button>
+                                @foreach ($hostnames as $hostname)
+                                    <tr>
+                                        <td>{{$hostname->fqdn}}</td>
+                                        <td>
+                                            <form id="suspend-form" method="POST" action='{{route('toggleApp',[$hostname->id])}}'>
+                                                {{ csrf_field() }}
+                                                @if(!$hostname->under_maintenance_since)
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-success"> ON </button>
+                                                </div>
+                                                @else
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-"> OFF </button>
+                                                </div>
+                                                @endif
+                                            </form>
+                                        </td>
+                                        <td class="text-right">
+                                            <div class="dropdown">
+                                                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-v"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                                        <form action="{{ route('deleteApp',[$hostname->id])}}" method="post">
+                                                            @csrf
+                                                            @method('delete')
+                                                            
+                                                            <button type="button" class="dropdown-item" onclick="confirm('{{ __("Are you sure you want to delete this Tenant?") }}') ? this.parentElement.submit() : ''">
+                                                                {{ __('Delete') }}
+                                                            </button>
+                                                        </form>    
+                                                </div>
                                             </div>
-                                            @else
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-"> OFF </button>
-                                            </div>
-                                            @endif
-                                        </form>
-                                    </th>
-                                    <td>
-                                        <form id="delete-form" method="POST" action='{{ route('deleteApp',[$hostname->id])}}'> 
-                                            {{ csrf_field() }}
-                                            {{ method_field('DELETE') }}
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-danger"> DELETE </button>
-                                            </div>
-                                        </form>
-                                    </td>
-                                </tr>
+                                        </td>
+                                    </tr>
                                 @endforeach
-                                   </tbody>
+                            </tbody>
                         </table>
+                    </div>
+                    <div class="card-footer py-4">
+                        <nav class="d-flex justify-content-end" aria-label="...">
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
         @include('layouts.footers.auth')
+    </div>
 @endsection
