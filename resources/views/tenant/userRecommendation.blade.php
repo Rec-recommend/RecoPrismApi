1c816425e918fe5435233624785dd551c089f09f
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-06-14 00:19:59 +0200

    Fix userRecommendation View

--- resources/views/tenant/userRecommendation.blade.php
+++ resources/views/tenant/userRecommendation.blade.php
@@ -1,46 +1,61 @@
-@extends('layouts.app')
+@extends('layouts.app', ['title' => __('Recommendations')])
 
 @section('content')
-@include('layouts.headers.cards')
-<br>
-<div class="container">
-    @if(isset($end_user))
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>User ID</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>{{$end_user->id}}</td>
-            </tr>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>items ID</th>
-                        @foreach ($attributes as $attribute)
-                        <th>{{$attribute->label}}</th>
-                        @endforeach
-                    </tr>
-                </thead>
-                <tbody>
-            @foreach ($items as $item)
-            <tr>
-                <td>{{$item}}</td> 
-                @foreach ($items_value as $key => $value)
-                    @if($value->item_id == $item)
-                        <td>{{$value->value}}</td> 
-                    @endif
-                @endforeach
-            </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </tbody>
-    </table>
-    @endif
+    @include('layouts.headers.cards')
 
-</div>
-@include('layouts.footers.auth')
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <h3 class="mb-0">{{ __('User Recommendatios Result') }}</h3>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-12">
+                        @if (session('status'))
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                {{ session('status') }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @endif
+                    </div>
+
+                    <div class="table-responsive">
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">{{ __('items ID') }}</th>
+                                    @foreach ($attributes as $attribute)
+                                    <th scope="col">{{$attribute->label}}</th>
+                                    @endforeach
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    @foreach ($items as $item)
+                                    <tr>
+                                        <td>{{$item}}</td> 
+                                        @foreach ($items_value as $key => $value)
+                                            @if($value->item_id == $item)
+                                                <td>{{$value->value}}</td> 
+                                            @endif
+                                        @endforeach
+                                    </tr>
+                                        @endforeach
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+        @include('layouts.footers.auth')
+    </div>
 @endsection
