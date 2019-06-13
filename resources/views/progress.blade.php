75cc0b42c710b33e064aec9568d46e6a26954c05
a-tarek <a.tarek.elsayed@gmail.com>
2019-06-13 15:51:18 +0200

    Fix Views Style

--- resources/views/progress.blade.php
+++ resources/views/progress.blade.php
@@ -22,226 +22,226 @@ $progress = '100';
 }
 @endphp
 <div class="container">
-    <div class="row"><br />
-        <div class="col-md-12">
-            <br>
-            <div class="progress">
-                <div class="one {{$color1}}-color"></div>
-                <div class="two {{$color2}}-color"></div>
-                <div class="three {{$color3}}-color"></div>
-                <div class="four {{$color4}}-color"></div>
-                <div class="progress-bar bg-success" style="width:{{$progress}}%;"></div>
-            </div>
-            <div class="row">
-                <div class="clickable col-3 text-{{$color1}}" data-toggle="modal" data-target="#addAttributes">
-                    Attributes
-                </div>
-                <div class="clickable col-3 text-{{$color2}}" data-toggle="modal" data-target="#csv">
-                    Users
-                </div>
-                <div class="clickable col-3 text-{{$color3}}" data-toggle="modal" data-target="#csv">
-                    Items
-                </div>
-                <div class="clickable col-3 text-{{$color4}}" data-toggle="modal" data-target="#csv">
-                    Ratings
-                </div>
-            </div>
-            <br>
+  <div class="row"><br />
+    <div class="col-md-12">
+      <br>
+      <div class="progress">
+        <div class="one {{$color1}}-color"></div>
+        <div class="two {{$color2}}-color"></div>
+        <div class="three {{$color3}}-color"></div>
+        <div class="four {{$color4}}-color"></div>
+        <div class="progress-bar bg-success" style="width:{{$progress}}%;"></div>
+      </div>
+      <div class="row">
+        <div class="clickable col-3 text-{{$color1}}" data-toggle="modal" data-target="#addAttributes">
+          Attributes
         </div>
-    </div>
-    <hr />
-</div>
-<div class="modal fade" id="addAttributes" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-                    <form method="POST" action="{{route('storeAttributes')}}" id="form">
-                            @csrf
-                            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-                          
-                            <div class="form-horizontal">
-                              <div class="form-body">
-                                <div class="form-group">
-                                  @if ($errors->any())
-                                  <div class="alert alert-danger" role="alert">
-                                      {{ $errors->first('label')}}
-                                      {{ $errors->first('weight')}}
-                                    </div>
-                                  @endif
-                                  
-                                  <label class="control-label col-md-3">Attributes & weights</label>
-                                  <div class="col-md-10 fields">
-                                    <div class="row">
-                                      <div class="col">
-                                        <input type="text" class="form-control" name='attribute_1' />
-                                      </div>
-                                      <div class="col">
-                                        <input type="number" class="form-control" name='attribute_1_weight' placeholder="Default weight 1" min="1"/>
-                                      </div>
-                                      
-                                        <span class="btn input-group-addon add-field btn-default col">+</span>
-                                    </div>
-                                    <span class="help-block">Enter List of Attributes </span>
-                                </div>
-                              </div>
-                            </div>
-                              <div class="form-actions right">
-                                <div class="row">
-                                  <div class="col-md-offset-3 col-md-9">
-                                    <button type="submit" class="btn btn-primary next-step">Submit</button>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </form>
-                          
-                          <script>
-                          
-                            var count = 1;
-                            $('.add-field').click(function () {
-                              count++;
-                              var template = '<div class="row"><div class="col"><input type="text" class="form-control" name="attribute_' + count + '"/></div>' +
-                                ' <div class="col"><input type="number" class="form-control" name="attribute_' + count + '_weight"/></div>',
-                                minusButton = '<span class="btn input-group-addon delete-field btn-danger col">-</span></div>';
-                              var temp = $(template).insertBefore('.help-block');
-                              temp.append(minusButton);
-                            });
-                          
-                            $('.fields').on('click', '.delete-field', function () {
-                              count--;
-                              $(this).parent().remove();
-                            });
-                          
-                            $(function () {
-                              $('form').submit(function () {
-                                $('#result').text(JSON.stringify($('form').serializeObject()));
-                                return false;
-                              });
-                            });
-                          </script>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
-          </div>
+        <div class="clickable col-3 text-{{$color2}}" data-toggle="modal" data-target="#csv">
+          Users
+        </div>
+        <div class="clickable col-3 text-{{$color3}}" data-toggle="modal" data-target="#csv">
+          Items
+        </div>
+        <div class="clickable col-3 text-{{$color4}}" data-toggle="modal" data-target="#csv">
+          Ratings
         </div>
       </div>
-      <div class="modal fade" id="csv" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
+      <br>
+    </div>
+  </div>
+  <hr />
+</div>
+<div class="modal fade" id="addAttributes" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Add Attributes</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="{{route('storeAttributes')}}" id="form">
+          @csrf
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+          <div class="form-horizontal">
+            <div class="form-body">
+              <div class="form-group">
+                @if ($errors->any())
+                <div class="alert alert-danger" role="alert">
+                  {{ $errors->first('label')}}
+                  {{ $errors->first('weight')}}
                 </div>
-                <div class="modal-body">
-                        <div class="container">
-                                <div class="card bg-light mt-3">
-                                    <div class="card-header">
-                                        Import your data
-                                    </div>
-                                    @if(Session::has('message'))
-                                        @if(Session::has('success'))
-                                            <p class="alert {{ Session::get('alert-class', 'alert-success') }}">{{ Session::get('message') }}</p>
-                                        @else
-                                            <p class="alert {{ Session::get('alert-class', 'alert-danger') }}">{{ Session::get('message') }}</p>
-                                        @endif
-                                    @endif
-                                    <div class="card-body">
-                                        <form action="{{ route('csv.store') }}" method="POST" enctype="multipart/form-data" name="csvForm">
-                                            @csrf
-                                            <select onChange="document.forms.csvForm.model.value = this.value" class="form-control input-group mb-1" >
-                                                    <option value="Item">items</option>
-                                                    <option value="User">users</option>
-                                                    <option value="Rating">ratings</option>
-                                                    <option value="Purchase">Purchases</option>
-                                            </select>
-                                            <input type="file" name="csvfile" class="form-control">
-                                            <input type="text" name="model" value='Item' class="form-control" hidden>
-                                            <br>
-                                            <button class="btn btn-success">Import CSV file</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>                              
-                              <script>
-                              
-                                var count = 1;
-                                $('.add-field').click(function () {
-                                  count++;
-                                  var template = '<div class="row"><div class="col"><input type="text" class="form-control" name="attribute_' + count + '"/></div>' +
-                                    ' <div class="col"><input type="number" class="form-control" name="attribute_' + count + '_weight"/></div>',
-                                    minusButton = '<span class="btn input-group-addon delete-field btn-danger col">-</span></div>';
-                                  var temp = $(template).insertBefore('.help-block');
-                                  temp.append(minusButton);
-                                });
-                              
-                                $('.fields').on('click', '.delete-field', function () {
-                                  count--;
-                                  $(this).parent().remove();
-                                });
-                              
-                                $(function () {
-                                  $('form').submit(function () {
-                                    $('#result').text(JSON.stringify($('form').serializeObject()));
-                                    return false;
-                                  });
-                                });
-                              </script>
+                @endif
+
+                <label class="control-label col-md-3">Attributes & weights</label>
+                <div class="col-md-10 fields">
+                  <div class="row">
+                    <div class="col">
+                      <input type="text" class="form-control" name='attribute_1' />
+                    </div>
+                    <div class="col">
+                      <input type="number" class="form-control" name='attribute_1_weight' placeholder="Default weight 1" min="1" />
+                    </div>
+
+                    <span class="btn input-group-addon add-field btn-default col">+</span>
+                  </div>
+                  <span class="help-block">Enter List of Attributes </span>
                 </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+            <div class="form-actions right">
+              <div class="row">
+                <div class="col-md-offset-3 col-md-9">
+                  <button type="submit" class="btn btn-primary next-step">Submit</button>
                 </div>
               </div>
             </div>
           </div>
+        </form>
+
+        <script>
+          var count = 1;
+          $('.add-field').click(function() {
+            count++;
+            var template = '<div class="row"><div class="col"><input type="text" class="form-control" name="attribute_' + count + '"/></div>' +
+              ' <div class="col"><input type="number" class="form-control" name="attribute_' + count + '_weight"/></div>',
+              minusButton = '<span class="btn input-group-addon delete-field btn-danger col">-</span></div>';
+            var temp = $(template).insertBefore('.help-block');
+            temp.append(minusButton);
+          });
+
+          $('.fields').on('click', '.delete-field', function() {
+            count--;
+            $(this).parent().remove();
+          });
+
+          $(function() {
+            $('form').submit(function() {
+              $('#result').text(JSON.stringify($('form').serializeObject()));
+              return false;
+            });
+          });
+        </script>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="csv" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Import CSVs</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="container">
+          <div class="card bg-light mt-3">
+            <div class="card-header">
+              Import your data
+            </div>
+            @if(Session::has('message'))
+            @if(Session::has('success'))
+            <p class="alert {{ Session::get('alert-class', 'alert-success') }}">{{ Session::get('message') }}</p>
+            @else
+            <p class="alert {{ Session::get('alert-class', 'alert-danger') }}">{{ Session::get('message') }}</p>
+            @endif
+            @endif
+            <div class="card-body">
+              <form action="{{ route('csv.store') }}" method="POST" enctype="multipart/form-data" name="csvForm">
+                @csrf
+                <select onChange="document.forms.csvForm.model.value = this.value" class="form-control input-group mb-1">
+                  <option value="Item">items</option>
+                  <option value="User">users</option>
+                  <option value="Rating">ratings</option>
+                  <option value="Purchase">Purchases</option>
+                </select>
+                <input type="file" name="csvfile" class="form-control">
+                <input type="text" name="model" value='Item' class="form-control" hidden>
+                <br>
+                <button class="btn btn-success">Import CSV file</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <script>
+          var count = 1;
+          $('.add-field').click(function() {
+            count++;
+            var template = '<div class="row"><div class="col"><input type="text" class="form-control" name="attribute_' + count + '"/></div>' +
+              ' <div class="col"><input type="number" class="form-control" name="attribute_' + count + '_weight"/></div>',
+              minusButton = '<span class="btn input-group-addon delete-field btn-danger col">-</span></div>';
+            var temp = $(template).insertBefore('.help-block');
+            temp.append(minusButton);
+          });
+
+          $('.fields').on('click', '.delete-field', function() {
+            count--;
+            $(this).parent().remove();
+          });
+
+          $(function() {
+            $('form').submit(function() {
+              $('#result').text(JSON.stringify($('form').serializeObject()));
+              return false;
+            });
+          });
+        </script>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 <style>
-    .one,
-    .two,
-    .three,
-    .four {
-        position: absolute;
-        margin-top: -10px;
-        margin-left: -50px;
-        z-index: 1;
-        height: 40px;
-        width: 40px;
-        border-radius: 25px;
+  .one,
+  .two,
+  .three,
+  .four {
+    position: absolute;
+    margin-top: -10px;
+    margin-left: -50px;
+    z-index: 1;
+    height: 40px;
+    width: 40px;
+    border-radius: 25px;
 
-    }
-    .clickable:hover{
-        cursor: pointer;
-    }
-    .one {
-        left: 25%;
-    }
+  }
 
-    .two {
-        left: 50%;
-    }
+  .clickable:hover {
+    cursor: pointer;
+  }
 
-    .three {
-        left: 75%;
-    }
+  .one {
+    left: 25%;
+  }
 
-    .four {
-        left: 100%;
-    }
+  .two {
+    left: 50%;
+  }
 
-    .primary-color {
-        background-color: #4989bd;
-    }
+  .three {
+    left: 75%;
+  }
 
-    .success-color {
-        background-color: #5cb85c;
-    }
+  .four {
+    left: 100%;
+  }
+
+  .primary-color {
+    background-color: #4989bd;
+  }
+
+  .success-color {
+    background-color: #5cb85c;
+  }
 </style>
 @include('layouts.footers.auth')
 @endsection
--- resources/views/tenant/subscription.blade.php
+++ resources/views/tenant/subscription.blade.php
@@ -1,34 +1,63 @@
-@extends('layouts.app')
+
+@extends('layouts.app', ['title' => __('Plan Management')])
 
 @section('content')
-    @include('layouts.headers.cards')
+@include('users.partials.header', ['title' => __('Plan Management')])
 
 
-    <form method="post" action="{{ route('subscription.swap') }}" autocomplete="off">
-                            @csrf
-    <div class="form-group"  style="padding:10px;width:500px;">
-                              <label class="btn btn-success" for="exampleFormControlSelect1">Change Selected Plan</label>
-                              <select class="form-control" id="exampleFormControlSelect1" name="plan_id">
-                                @foreach ($plans as $plan)
-                              <option value={{$plan->stripe_plan}}> 
-                                  {{$plan->name}} : {{$plan->cost}}$</option>
-                                @endforeach
-                              </select>
-                       <div class="text-center">
-                                    <button type="submit" class="btn btn-success mt-4">{{ __('SUBMIT') }}</button>
-                                </div>  
-                                @if($client->status == 1)
-                                <div>
-                                    <a href="{{ route('subscription.unsubscribe')}}" class="btn btn-danger mt-4">{{ __('UNSUBSCRIBE') }}</a>
-                                </div>   
-                                @else
-                                <div>
-                                    <a href="{{ route('subscription.resume')}}" class="btn btn-success mt-4">{{ __('RESUME SUBSCRIPTION') }}</a>
-                                </div>
-                                @endif
-                                    
-                         
-                            </div>   
-                                     
-@include('layouts.footers.auth')
+<div class="container-fluid mt--7">
+  <div class="row">
+    <div class="col">
+      <div class="card shadow">
+        <div class="card-header border-0">
+          <div class="row align-items-center">
+            <div class="col-8">
+              <h3 class="mb-0">{{ __('Plan') }}</h3>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12">
+          @if (session('status'))
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('status') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          @endif
+        </div>
+
+        <div class="card-body">        
+          <form method="post" action="{{ route('subscription.swap') }}" autocomplete="off">
+            @csrf
+            <div class="form-group" style="padding:10px;width:500px;">
+              <select class="form-control input-group mb-1" id="exampleFormControlSelect1" name="plan_id">
+                @foreach ($plans as $plan)
+                <option value={{$plan->stripe_plan}}>
+                  {{$plan->name}} : {{$plan->cost}}$</option>
+                @endforeach
+              </select>
+              <div class="text-center">
+                <button type="submit" class="btn btn-success mt-4">{{ __('SUBMIT') }}</button>
+              </div>
+              @if($client->status == 1)
+              <div>
+                <a href="{{ route('subscription.unsubscribe')}}" class="btn btn-danger mt-4">{{ __('UNSUBSCRIBE') }}</a>
+              </div>
+              @else
+              <div>
+                <a href="{{ route('subscription.resume')}}" class="btn btn-success mt-4">{{ __('RESUME SUBSCRIPTION') }}</a>
+              </div>
+              @endif
+
+
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  @include('layouts.footers.auth')
+</div>
 @endsection
